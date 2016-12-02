f68dc81c4ac4557fd7d655d3697a2fa33dd0b044
Jemoeder <flunk@icebox.nl>
2016-12-02 00:42:24 +0000

    Dark theme

--- js/ui/baseapp.js
+++ js/ui/baseapp.js
@@ -1,9 +1,5 @@
 class BaseApp{
   	constructor(icon){
-      	this.init(icon);
-    }
-  
-  	init(icon){
         this.view = new View( );
       	this.item = new NavBarItem( new Glyphicon(icon), app.navBarApp.navBar, this.view );
     }
