cc9aab18f28b91d016480eb16f5bd75cf0c88b55
Jemoeder <flunk@icebox.nl>
2017-02-06 07:28:20 +0000

    Highlightable tabs

--- js/bootstrap/dropdown.js
+++ js/bootstrap/dropdown.js
@@ -2,8 +2,8 @@ class Dropdown extends List {
   	constructor(x,y){
 		super("dropdown-menu unselectable");
         this.element.style.display = 'block';
-      	this.top = x;
-      	this.left = y;
+      	this.x = x;
+      	this.y = y;
       	
       	this.addItem = function( caption, callback ){
         	let item = new DropdownItem(this, caption, callback );
